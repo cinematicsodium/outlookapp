c5485496925c65c669d88ca43614a16e766f7892
CinematicSodium <segues_idlers0p@icloud.com>
2026-09-16 17:33:03 -0600

    fix: add type ignore comment for ModelT TypeVar in types.py

--- types.py
+++ types.py
@@ -4,5 +4,5 @@ from typing import TypeAlias, TypeVar
 LowerStr: TypeAlias = str
 StrPath: TypeAlias = str | Path
 T = TypeVar("T", covariant=True)  # ruff: ignore[PLC0105]
-ModelT = TypeVar("ModelT", bound="ItemModel")
+ModelT = TypeVar("ModelT", bound="ItemModel")  # type: ignore # ruff: ignore[F821]
 RawT = TypeVar("RawT")
